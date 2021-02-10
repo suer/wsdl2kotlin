e7937fd922cf12978f15d519a884c2f44b245168
mallowlabs <mallowlabs@gmail.com>
2021-02-10 22:18:46 +0900

    Introduce Main.kt

--- wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/WSDL2Kotlin.kt
+++ wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/WSDL2Kotlin.kt
@@ -1,64 +1,64 @@
 package org.codefirst.wsdl2kotlin
 
-fun main() {
-    // TODO: from args
-    val path = WSDLDefinitions::class.java.getResource("/tempconvert.wsdl.xml").file
-
-    val wsdl = WSDL.parse(path)
-    var kotlin = """
+class WSDL2Kotlin() {
+    fun run(vararg paths: String): String {
+        var kotlin = """
 import org.codefirst.wsdl2kotlin.WSDLService
 import org.codefirst.wsdl2kotlin.XMLParam
 import org.codefirst.wsdl2kotlin.XSDType
+"""
+        paths.map { WSDL.parse(it) }.forEach { wsdl ->
 
+            kotlin += """
 class ${wsdl.service.name}(val endpoint: String) : WSDLService() {
 """
-    wsdl.portTypes.forEach { portType ->
-        portType.operations.forEach { operation ->
-            val inputType = wsdl.findType(operation.input.message)
-            val outputType = wsdl.findType(operation.output.message)
-            if (inputType != null && outputType != null) {
-                kotlin += """
+            wsdl.portTypes.forEach { portType ->
+                portType.operations.forEach { operation ->
+                    val inputType = wsdl.findType(operation.input.message)
+                    val outputType = wsdl.findType(operation.output.message)
+                    if (inputType != null && outputType != null) {
+                        kotlin += """
     fun request(parameters: $inputType): $outputType {
         return requestGeneric<$inputType, $outputType>(parameters)
     }
 """
+                    }
+                }
             }
-        }
-    }
 
-    kotlin += """
+            kotlin += """
 }
 """
 
-    wsdl.types.schema.elements.filter { it.complexType != null }.forEach { element ->
-        kotlin += """
-class ${wsdl.service.name}_${element.name} ("""
-        element.complexType?.sequences?.forEach { sequence ->
-            sequence.elements.forEach { element2 ->
+            wsdl.types.schema.elements.filter { it.complexType != null }.forEach { element ->
                 kotlin += """
+class ${wsdl.service.name}_${element.name} ("""
+                element.complexType?.sequences?.forEach { sequence ->
+                    sequence.elements.forEach { element2 ->
+                        kotlin += """
     var ${element2.name}: ${element2.typeInKotlin()},"""
-            }
-        }
+                    }
+                }
 
-        kotlin += """
+                kotlin += """
 ) : XSDType() {
     override fun xmlParams(): Array<XMLParam> {
         return arrayOf("""
-        element.complexType?.sequences?.forEach { sequence ->
-            sequence.elements.forEach { element2 ->
-                // TODO: tns or empty
-                kotlin += """
+                element.complexType?.sequences?.forEach { sequence ->
+                    sequence.elements.forEach { element2 ->
+                        // TODO: tns or empty
+                        kotlin += """
                 XMLParam("tns", "${element2.name}", ${element2.name}),
 """
-            }
-        }
-        kotlin += """
+                    }
+                }
+                kotlin += """
         )
     }
 }
 """
+            }
+        }
+        return kotlin
     }
-
-    // TODO: write to file
-    println(kotlin)
 }
