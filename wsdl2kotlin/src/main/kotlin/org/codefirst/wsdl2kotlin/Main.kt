8cdd444b202b3868aa8386ed2a75b6e835faabee
Ryo SUETSUGU <suetsugu.r@gmail.com>
2021-03-28 23:31:50 +0900

    refine cli options using Clikt

--- wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/Main.kt
+++ wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/Main.kt
@@ -1,9 +1,27 @@
 package org.codefirst.wsdl2kotlin
 
-fun main(args: Array<String>) {
-    val outputs = WSDL2Kotlin().run(*args)
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.arguments.unique
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
+import java.nio.file.Path
 
-    outputs.forEach {
-        it.save()
+class Main : CliktCommand() {
+    val dir: String by option("-d", "--dir", help = "output directory").default("./")
+    val paths: Set<Path> by argument().path(mustExist = true).multiple().unique()
+
+    override fun run() {
+        val outputs = WSDL2Kotlin().run(*paths.map { it.toString() }.toTypedArray())
+
+        outputs.forEach {
+            it.save(dir)
+        }
     }
+}
+
+fun main(args: Array<String>) {
+    Main().main(args)
 }
