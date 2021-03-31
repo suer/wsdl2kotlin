b78310f75866d3e420f328cac1dde85b37b2f162
mallowlabs <mallowlabs@gmail.com>
2021-03-31 21:07:55 +0900

    Replace sample WSDL and XSD

--- wsdl2kotlin/src/test/kotlin/org/codefirst/wsdl2kotlin/XSDTest.kt
+++ wsdl2kotlin/src/test/kotlin/org/codefirst/wsdl2kotlin/XSDTest.kt
@@ -6,20 +6,20 @@ import kotlin.test.assertEquals
 class XSDTest {
     @Test
     fun testParse() {
-        val path = this::class.java.getResource("/weatherforecast.xsd.xml").file
+        val path = this::class.java.getResource("/sample.xsd.xml").file
         val xsd = XSD.parse(path)
-        assertEquals(5, xsd.elements.size)
+        assertEquals(30, xsd.elements.size)
     }
 
     @Test
     fun testIsXSDTrue() {
-        val path = this::class.java.getResource("/weatherforecast.xsd.xml").file
+        val path = this::class.java.getResource("/sample.xsd.xml").file
         assertEquals(true, XSD.isXSD(path))
     }
 
     @Test
     fun testIsXSDFalse() {
-        val path = this::class.java.getResource("/weatherforecast.wsdl.xml").file
+        val path = this::class.java.getResource("/sample.wsdl.xml").file
         assertEquals(false, XSD.isXSD(path))
     }
 }
