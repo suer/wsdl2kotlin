2f8ee18510bf635178ef974acd264c47ac121086
mallowlabs <mallowlabs@gmail.com>
2026-07-04 22:32:53 +0900

    Use Charsets instead of Charset.forName

--- wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/Output.kt
+++ wsdl2kotlin/src/main/kotlin/org/codefirst/wsdl2kotlin/Output.kt
@@ -1,7 +1,6 @@
 package org.codefirst.wsdl2kotlin
 
 import java.io.File
-import java.nio.charset.Charset
 
 class Output(
     private val serviceName: String,
@@ -15,6 +14,6 @@ class Output(
         }
         val file = File(directory, "$serviceName.kt")
         println("Generating ${file.canonicalPath} ...")
-        file.writeText(code, Charset.forName("UTF-8"))
+        file.writeText(code, Charsets.UTF_8)
     }
 }
