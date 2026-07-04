2ffd9798917e20369c5db057374034962ff334a7
mallowlabs <mallowlabs@gmail.com>
2026-07-04 21:30:15 +0900

    Remove DTD awareness

--- wsdl2kotlin-runtime/src/main/kotlin/org/codefirst/wsdl2kotlin/DocumentHelper.kt
+++ wsdl2kotlin-runtime/src/main/kotlin/org/codefirst/wsdl2kotlin/DocumentHelper.kt
@@ -1,6 +1,7 @@
 package org.codefirst.wsdl2kotlin
 
 import org.w3c.dom.Element
+import javax.xml.XMLConstants
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.OutputKeys
@@ -13,13 +14,25 @@ class DocumentHelper {
             DocumentBuilderFactory
                 .newInstance()
                 .apply {
-                    this.isNamespaceAware = true
+                    isNamespaceAware = true
+                    isXIncludeAware = false
+                    isExpandEntityReferences = false
+                    setFeature("http://apache.org/xml/features/disallow-doctype-decl", true)
+                    setFeature("http://xml.org/sax/features/external-general-entities", false)
+                    setFeature("http://xml.org/sax/features/external-parameter-entities", false)
+                    setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false)
+                    setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true)
                 }.newDocumentBuilder()
 
         fun newTransformer(): Transformer =
-            TransformerFactory.newInstance().newTransformer().apply {
-                this.setOutputProperty(OutputKeys.INDENT, "yes")
-            }
+            TransformerFactory
+                .newInstance()
+                .apply {
+                    setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true)
+                }.newTransformer()
+                .apply {
+                    this.setOutputProperty(OutputKeys.INDENT, "yes")
+                }
 
         fun getChildElementsByTagName(
             parentElement: Element,
